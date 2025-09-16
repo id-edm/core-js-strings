00323adc01d250dbf65a8c858eb20a730ee6a3c3
id-edm <ivandubochit@gmail.com>
2025-09-16 11:33:11 +0400

    feat: add solution task removeLastOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -168,8 +168,12 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  if (str.includes(value) === false) return str;
+  return (
+    str.slice(0, str.lastIndexOf(value)) +
+    str.slice(str.lastIndexOf(value) + value.length)
+  );
 }
 
 /**
