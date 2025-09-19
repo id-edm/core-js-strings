78a7deb9c32b3be1712863d3eb25c59a832fc3fc
id-edm <ivandubochit@gmail.com>
2025-09-19 13:38:38 +0400

    feat: add solution task invertCase

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -368,8 +368,15 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((letter) =>
+      letter === letter.toUpperCase()
+        ? letter.toLowerCase()
+        : letter.toUpperCase()
+    )
+    .join('');
 }
 
 /**
