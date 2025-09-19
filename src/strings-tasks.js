19acd6c65c6e74c6a7c4488ecf150c8d22bfc722
id-edm <ivandubochit@gmail.com>
2025-09-19 12:33:10 +0400

    feat: add solution task countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -298,8 +298,9 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const onlyVowels = str.replace(/[^aeiouy]/gi, '');
+  return onlyVowels.length;
 }
 
 /**
